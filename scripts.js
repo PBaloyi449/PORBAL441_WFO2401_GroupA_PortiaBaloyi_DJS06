75c8c4f4f4b780d6d955ac17902c12c26338ff4f
PBaloyi449 <pbaloyi449@gmail.com>
2024-05-23 15:01:36 +0200

    Name lengths

--- scripts.js
+++ scripts.js
@@ -25,3 +25,7 @@ names.forEach((name, index) => console.log(`${name} (${provinces[index]})`));
 //2. Uppercase Transformation
 const upperCaseProvinces = provinces.map(province => province.toUpperCase());
 console.log(upperCaseProvinces);
+
+//3. Name Lengths
+const nameLengths = names.map(name => name.length);
+console.log(nameLengths);
